37574eb09ac6fbaf451f4bf264ff5ed09d9b73a4
IvanTsvetkov-dev <vvvanusha62@mail.ru>
2025-12-07 10:58:40 +0300

    Расширена модель и схема для сущности Dog

--- backend/src/models/dogs.py
+++ backend/src/models/dogs.py
@@ -1,5 +1,9 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from src.database import Base
+from src.enums import GenderEnum
+from sqlalchemy import String
+from datetime import datetime
+from sqlalchemy import Text
 
 
 class DogModel(Base):
@@ -8,3 +12,8 @@ class DogModel(Base):
     id : Mapped[int] = mapped_column(primary_key=True)
     name : Mapped[str]
     age : Mapped[int]
+    breed: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str] = mapped_column(Text())
+    intake_date: Mapped[datetime]
+    veterinary_passport: Mapped[bool]
+    gender: Mapped[GenderEnum]
--- backend/src/schemas/dogs.py
+++ backend/src/schemas/dogs.py
@@ -1,10 +1,19 @@
 from pydantic import BaseModel
+from datetime import datetime
+
+from src.enums import GenderEnum
+
 
 class DogAddSchema(BaseModel):
     name: str
     age: int
 
 class DogGetSchema(BaseModel):
-    id : int
+    id: int
     name: str
     age: int
+    breed: str
+    description: str
+    intake_date: datetime
+    veterinary_passport: bool
+    gender: GenderEnum
