1c194e9b7e849a03e7ec224db9c9fde6b191573d
Dima Chukhlancev <htmlweb78@gmail.com>
2025-12-09 19:44:36 +0300

    Сделал страницу логина

--- frontend/app/components/TextInput.vue
+++ frontend/app/components/TextInput.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
 
-const { placeholder = "", name = "", inputId = "" } = defineProps<{
-    placeholder?: string,
+const { placeholder = "", name = "", inputId = "", type = "text" } = defineProps<{
+    placeholder?: string
     name?: string
     inputId?: string
+    type?: string
 }>()
 
 const model = defineModel<string>()
@@ -18,8 +19,8 @@ function focusInput() {
 </script>
 
 <template>
-    <div class="border border-input-border px-4 py-2 rounded-xl hover:cursor-text" @click="focusInput">
-        <input :id="inputId" :name="name" ref="input" v-model="model" :placeholder="placeholder" type="text"
+    <div class="border border-input-border px-4 py-2 rounded-lg hover:cursor-text" @click="focusInput">
+        <input :type="type" :id="inputId" :name="name" ref="input" v-model="model" :placeholder="placeholder"
             class="focus:outline-0 w-full text-text-secondary">
     </div>
 </template>
