30aee064ec8df85d12e9e210a7ff379559b96154
Dima Chukhlancev <htmlweb78@gmail.com>
2025-12-11 18:47:50 +0300

    Добавил компноненты чекбокс с label и радио с label, сделал страницу помощь приюту, начал доделывать header

---
+++ frontend/app/components/LabeledCheckbox.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { name = "" } = defineProps<{
+    name?: string
+}>()
+</script>
+<template>
+    <label class="flex gap-2 items-center text-text-secondary">
+        <input class="checkbox" type="checkbox" :name="name">
+        <slot></slot>
+    </label>
+</template>
