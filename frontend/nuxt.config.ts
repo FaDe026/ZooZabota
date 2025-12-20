5f94fbc7157abc336af9f878bb6098eb05a37595
Dmitriy Chukhlantsev <htmlweb78@gmail.com>
2025-12-20 14:34:37 +0300

    Поменял структуру файлов

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,18 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  vite: { plugins: [tailwindcss(),], },
-  css: ['./app/assets/css/main.css'],
-  modules: ['@nuxtjs/google-fonts', '@nuxt/icon'],
+  vite: { plugins: [tailwindcss()] },
+  css: ["./app/assets/css/main.css"],
+  modules: ["@nuxtjs/google-fonts", "@nuxt/icon"],
   googleFonts: {
     families: {
       Cuprum: true,
-    }
+    },
   },
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
   icon: {
-    mode: 'svg',
-    cssLayer: 'base'
-  }
-})
+    mode: "svg",
+    cssLayer: "base",
+  },
+});
