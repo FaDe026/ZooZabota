03ba70c051b715700535604666b7733949634478
IvanTsvetkov-dev <vvvanusha62@mail.ru>
2025-12-25 22:10:54 +0300

    Включён CORS. Добавлена раздача статики

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from src.api import main_router
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins = ["*"],
+  allow_methods = ["*"],
+  allow_headers = ["*"]
+)
+
 app.include_router(main_router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
